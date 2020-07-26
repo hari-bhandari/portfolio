2af27c6ae8a3bae4744adf2b24b676e1d4ce005e
hari <nepal9841921783@gmail.com>
2020-07-26 15:43:12 +0100

    added lazy load to navbar and welcome page

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
-import React, {Fragment, Suspense, lazy, useState,useEffect} from 'react';
+import React, {Fragment, Suspense, lazy, useState, useEffect} from 'react';
 
-import Navbar from "./Components/layout/Navbar";
 import './bootstrap.css'
-import Footer from "./Components/layout/Footer";
-import ContactMe from "./Components/sections/About Me/ContactMe";
-import WecomePage from "./Components/sections/Welcome /WecomePage";
+const Footer = lazy(() => import('./Components/layout/Footer'))
+const ContactMe = lazy(() => import('./Components/sections/About Me/ContactMe'))
+const Navbar = lazy(() => import('./Components/layout/Navbar'))
+const WelcomePage = lazy(() => import('./Components/sections/Welcome /WecomePage'))
 const AboutMe = lazy(() => import('./Components/sections/About Me/AboutMe'))
-
 const Projects = lazy(() => import('./Components/sections/Projects/Projects'))
 const Skills = lazy(() => import('./Components/sections/About Me/Skills'))
 
 
 function App() {
-    const[loaded,setLoaded]=useState(false)
+    const [loaded, setLoaded] = useState(false)
     useEffect(() => {
         const timer = setTimeout(() => {
             setLoaded(true)
-        }, 200);
+        }, 50);
         return () => clearTimeout(timer);
     }, []);
 
     return (
-                <Fragment>
-                    <Navbar/>
-                    <WecomePage />
-                    <Suspense fallback={<div>loading...</div>}>
-                        {!loaded?(<div>Loading...</div>):(
-                            <div>
-                                <AboutMe />
-                                <Skills />
-                                <Projects />
-                                <ContactMe/>
-                            </div>
-                        )}
+        <Fragment>
+            <Suspense fallback={<div>loading...</div>}>
+                {!loaded ? (<div>Loading...</div>) : (
+                    <div>
+                         <Navbar/>
+                        <WelcomePage/>
+                        <AboutMe/>
+                        <Skills/>
+                        <Projects/>
+                        <ContactMe/>
+                        <Footer/>
+                    </div>
+                )}
 
 
-                    </Suspense>
-                    <Footer />
-                </Fragment>
+            </Suspense>
+
+        </Fragment>
     );
 }
 
