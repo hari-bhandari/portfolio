447096f88ef7a7b2780e40c75d02ffcc1eaf58d7
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-08 09:36:33 +0100

    added broteli plugin and bundle analyser

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,24 @@
 const CompressionPlugin = require('compression-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BrotliPlugin = require('brotli-webpack-plugin');
 
 module.exports = {
         plugins: [
-    new CompressionPlugin({
+    new CompressionPlugin({ //gzip plugin
         filename: '[path].gz[query]',
         algorithm: 'gzip',
         test: /\.(js|css|html|svg)$/,
         threshold: 8192,
         minRatio: 0.8
-    })
+    }),
+    new BrotliPlugin({ //brotli plugin
+        asset: '[path].br[query]',
+        test: /\.(js|css|html|svg)$/,
+        threshold: 10240,
+        minRatio: 0.8
+    }),
+    new BundleAnalyzerPlugin()
+
 ]
+
 }
