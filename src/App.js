072cd2f06051aa0317ee5cf322b7be1a3e34fea8
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-08 16:04:04 +0100

    experimenting with react lazy

--- src/App.js
+++ src/App.js
@@ -2,52 +2,31 @@ import React, {Fragment, lazy, Suspense,useEffect,useRef,useState} from 'react';
 
 import './bootstrap.css'
 import Navbar from "./Components/layout/Navbar";
-import WelcomePage from "./Components/sections/Welcome /WelcomePage";
-import AboutMe from "./Components/sections/About Me/AboutMe";
 const ContactMe = lazy(() => import('./Components/sections/About Me/ContactMe'))
+const WelcomePage = lazy(() => import('./Components/sections/Welcome /WelcomePage'))
+const AboutMe = lazy(() => import('./Components/sections/About Me/AboutMe'))
 const Projects = lazy(() => import('./Components/sections/Projects/Projects'))
 const Skills = lazy(() => import('./Components/sections/About Me/Skills'))
 const Footer = lazy(() => import('./Components/layout/Footer'))
-const useOnScreen = (options) => {
-    const [visible, setVisible] = useState(false)
-    const ref = useRef()
-    useEffect(() => {
-        const observer = new IntersectionObserver(([entry]) => {
-            if (visible) {
-            } else {
-                setVisible(entry.isIntersecting)
-            }
 
-        },options)
-        if(ref.current){
-            observer.observe(ref.current)
-        }
-
-        return ()=>{
-            if(ref.current){
-                observer.unobserve(ref.current)
-            }
-        }
-    },[ref,options])
-    return [ref,visible]
-}
 function App() {
 
-    const [ref,visible]=useOnScreen({rootMargin:'0px','threshold':'0.75'})
     return (
         <Fragment>
             <Navbar/>
+            <Suspense fallback={<div>Loading.. </div>}>
             <WelcomePage/>
-            {window.innerWidth<960&&<div ref={ref}></div>}
+            </Suspense>
+            <Suspense fallback={<div>Loading... </div>}>
             <AboutMe/>
-            {window.innerWidth>=960&&<div ref={ref}></div>}
-            <Suspense fallback={<div>. </div>}>
-                {visible && <Skills/>}
             </Suspense>
-            <Suspense fallback={<div>. </div>}>
-                {visible && (<Projects/>)}
+            <Suspense fallback={<div></div>}>
+                {<Skills/>}
             </Suspense>
-            <Suspense fallback={<div>. </div>}>
+            <Suspense fallback={<div></div>}>
+                <Projects/>
+            </Suspense>
+            <Suspense fallback={<div></div>}>
                 <ContactMe/>
             </Suspense>
 
