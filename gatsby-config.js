6d7d4fcfdd7a5ad8dc510694604bf51711c44c76
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-11-14 05:40:58 +0000

    changed meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Hari Bhandari`,
+    description: `A freelancer,full stack developer and mathematician `,
+    author: `Hari Bhandari`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
